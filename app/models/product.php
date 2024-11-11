3a89b682fd241dfaebea417aafff1f7ee2fbb48c
studentlucas <338362@student.mboutrecht.nl>
2024-11-11 20:41:10 +0100

    6de commit

--- app/models/product.php
+++ app/models/product.php
@@ -1,1 +1,26 @@
+// models/Product.php
+<?php
 
+class Product
+{
+    private $pdo;
+
+    public function __construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function getAllProducts()
+    {
+        $stmt = $this->pdo->query("SELECT * FROM Product ORDER BY Barcode ASC");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getProductById($id)
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM Product WHERE Id = :id");
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
+ 
