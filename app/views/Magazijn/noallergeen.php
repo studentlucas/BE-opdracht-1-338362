5c990c2fc7bdbab2ec8b97ab6389338721531cc4
studentlucas <338362@student.mboutrecht.nl>
2024-11-11 21:28:42 +0100

    14de commit

---
+++ app/views/Magazijn/noallergeen.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Allergenen Informatie</title>
+</head>
+<body>
+    <p>In dit product zitten geen stoffen die een allergische reactie kunnen veroorzaken.</p>
+    <script>
+        setTimeout(function(){
+            window.location.href = 'overview.php'; // Replace with the actual URL of the overview page
+        }, 4000);
+    </script>
+</body>
+</html>
